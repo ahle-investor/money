69b86fb6344dc9d42127f9fac36b985375c9b6de
Aitiim <56004528+aitiim-company@users.noreply.github.com>
2024-02-19 11:15:05 +0100

    hoang

---
+++ coin.py
@@ -0,0 +1,16 @@
+# Import libraries 
+import json 
+import requests 
+  
+# defining key/request url 
+btc = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+wld = "https://api.binance.com/api/v3/ticker/price?symbol=WLDUSDT"
+ada = "https://api.binance.com/api/v3/ticker/price?symbol=ADAUSDT"
+  
+# requesting data from url 
+data = requests.get(ada).json()
+print(f"{data['symbol']} price is {data['price']}")
+
+data = requests.get(wld).json()
+print(f"{data['symbol']} price is {data['price']}")
+
--- saving.py
+++ saving.py
@@ -1,1 +1,24 @@
+from datetime import datetime
 
+class 
+
+class Saving:
+    def __init__(self, amount, interest):
+        self.amount   = amount
+        self.interest = interest
+    def open(self, start):
+        self.start = start
+
+    def getInterest(self):
+        pass
+
+
+class Asset:
+    pass
+
+
+
+
+livretA = Saving(22000, 0.03)
+livretA.open(datetime.now())
+print(livretA)
