9e032d2c68a0d738c12fe7a304685ef4db0c795b
Aitiim <56004528+aitiim-company@users.noreply.github.com>
2024-03-10 19:14:00 +0000

    Mar 10, 2024, 8:14 PM

--- coin.py
+++ coin.py
@@ -18,5 +18,7 @@ print(f"{data['symbol']} price is {data['price']}")
 buyPrice = 0.000249567
 amount = 400693.86
 data = requests.get(floki).json()
-print(f"{data['symbol']} price is {data['price']} pl={(data['price']-buyPrice)*amount}")
+pl = (data['price']-buyPrice)*amount
+# print(f"{data['symbol']} price is f{data['price']} pl=f{pl}")
+print(pl)
 
