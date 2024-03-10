57d769029f7faeca4cfdae0f6a6f56fa9cd36311
Aitiim <56004528+aitiim-company@users.noreply.github.com>
2024-03-10 18:44:05 +0000

    Mar 10, 2024, 7:44 PM

--- coin.py
+++ coin.py
@@ -6,11 +6,15 @@ import requests
 btc = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
 wld = "https://api.binance.com/api/v3/ticker/price?symbol=WLDUSDT"
 ada = "https://api.binance.com/api/v3/ticker/price?symbol=ADAUSDT"
+floki = "https://api.binance.com/api/v3/ticker/price?symbol=FLOKIUSDT"
   
 # requesting data from url 
 data = requests.get(ada).json()
 print(f"{data['symbol']} price is {data['price']}")
 
 data = requests.get(wld).json()
+print(f"{data['symbol']} price is {data['price']}")
+
+data = requests.get(floki).json()
 print(f"{data['symbol']} price is {data['price']}")
 
