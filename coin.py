7894b091d3da62866d1b354cdf8e0b50aed57235
Aitiim <56004528+aitiim-company@users.noreply.github.com>
2024-03-10 19:39:11 +0000

    Mar 10, 2024, 8:39 PM

--- coin.py
+++ coin.py
@@ -7,7 +7,8 @@ btc = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
 wld = "https://api.binance.com/api/v3/ticker/price?symbol=WLDUSDT"
 ada = "https://api.binance.com/api/v3/ticker/price?symbol=ADAUSDT"
 floki = "https://api.binance.com/api/v3/ticker/price?symbol=FLOKIUSDT"
-mana = "https://api.binance.com/api/v3/ticker/price?symbol=FLOKIUSDT"
+mana = "https://api.binance.com/api/v3/ticker/price?symbol=MANAUSDT"
+bonk = "https://api.binance.com/api/v3/ticker/price?symbol=BONKUSDT"
   
 # requesting data from url 
 data = requests.get(ada).json()
@@ -24,10 +25,10 @@ pl = (float(data['price'])-buyPrice)*amount
 print(f"{data['symbol']} price is f{data['price']} pl=f{pl}")
 print(pl)
 
-# 
-buyPrice = 0.000249567
-amount = 400693.86
-data = requests.get(floki).json()
+# Bonk
+buyPrice = 0.0000326677
+amount = 40000000
+data = requests.get(bonk).json()
 pl = (float(data['price'])-buyPrice)*amount
 print(f"{data['symbol']} price is f{data['price']} pl=f{pl}")
 print(pl)
