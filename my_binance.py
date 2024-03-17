6469731ad9c01665ea2138c92579e005231d3444
Aitiim <56004528+aitiim-company@users.noreply.github.com>
2024-03-17 20:23:25 +0000

    Mar 17, 2024, 9:23 PM

--- my_binance.py
+++ my_binance.py
@@ -1,99 +1,11 @@
 import pandas as pd
 from datetime import datetime, timezone, timedelta
-import calendar
+import calendar, os
 import json
 import datetime as dt              # working with dates
 import requests
+# init
+api_key = os.environ.get('binance_api')
+api_secret = os.environ.get('binance_secret')
 
-def get_klines_iter(symbol, interval, start, end = None, limit=1000):
-    # start and end must be isoformat YYYY-MM-DD
-    # We are using utc time zone
-
-    # the maximum records is 1000 per each Binance API call
-
-    df = pd.DataFrame()
-
-    if start is None:
-        print('start time must not be None')
-        return
-    start = calendar.timegm(datetime.fromisoformat(start).timetuple()) * 1000
-
-    if end is None:
-        dt = datetime.now(timezone.utc)
-        utc_time = dt.replace(tzinfo=timezone.utc)
-        end = int(utc_time.timestamp()) * 1000
-        return
-    else:
-        end = calendar.timegm(datetime.fromisoformat(end).timetuple()) * 1000
-    last_time = None
-
-    while len(df) == 0 or (last_time is not None and last_time < end):
-        url = 'https://api.binance.com/api/v3/klines?symbol=' + \
-              symbol + '&interval=' + interval + '&limit=1000'
-        if(len(df) == 0):
-            url += '&startTime=' + str(start)
-        else:
-            url += '&startTime=' + str(last_time)
-
-        url += '&endTime=' + str(end)
-        df2 = pd.read_json(url)
-        df2.columns = ['Opentime', 'Open', 'High', 'Low', 'Close', 'Volume', 'Closetime',
-                       'Quote asset volume', 'Number of trades', 'Taker by base', 'Taker buy quote', 'Ignore']
-        dftmp = pd.DataFrame()
-        dftmp = pd.concat([df2, dftmp], axis=0, ignore_index=True, keys=None)
-
-        dftmp.Opentime = pd.to_datetime(dftmp.Opentime, unit='ms')
-        dftmp['Date'] = dftmp.Opentime.dt.strftime("%d/%m/%Y")
-        dftmp['Time'] = dftmp.Opentime.dt.strftime("%H:%M:%S")
-        dftmp = dftmp.drop(['Quote asset volume', 'Closetime', 'Opentime',
-                      'Number of trades', 'Taker by base', 'Taker buy quote', 'Ignore'], axis=1)
-        column_names = ["Date", "Time", "Open", "High", "Low", "Close", "Volume"]
-        dftmp.reset_index(drop=True, inplace=True)
-        dftmp = dftmp.reindex(columns=column_names)
-        string_dt = str(dftmp['Date'][len(dftmp) - 1]) + 'T' + str(dftmp['Time'][len(dftmp) - 1]) + '.000Z'
-        utc_last_time = datetime.strptime(string_dt, "%d/%m/%YT%H:%M:%S.%fZ")
-        last_time = (utc_last_time - datetime(1970, 1, 1)) // timedelta(milliseconds=1)
-        df = pd.concat([df, dftmp], axis=0, ignore_index=True, keys=None)
-
-    df.to_csv(str(symbol)+'.csv', sep='\t', index=False)
-
-def getPrices(symbol, date):
-    get_klines_iter(symbol+'USDT', '1h', '2024-03-15', '2024-03-16')
-
-# get_klines_iter('WLDUSDT', '30m', '2024-02-15', '2024-02-16')
-get_klines_iter('BNBUSDT', '1h', '2024-03-15', '2024-03-16')
-getPrices('FET', None)
-print("end")
-
-def get_binance_bars(symbol, interval, startTime, endTime):
- 
-    url = "https://api.binance.com/api/v3/klines"
- 
-    startTime = str(int(startTime.timestamp() * 1000))
-    endTime = str(int(endTime.timestamp() * 1000))
-    limit = '1000'
- 
-    req_params = {"symbol" : symbol, 'interval' : interval, 'startTime' : startTime, 'endTime' : endTime, 'limit' : limit}
- 
-    df = pd.DataFrame(json.loads(requests.get(url, params = req_params).text))
- 
-    if (len(df.index) == 0):
-        return None
-     
-    df = df.iloc[:, 0:6]
-    df.columns = ['datetime', 'open', 'high', 'low', 'close', 'volume']
- 
-    df.open      = df.open.astype("float")
-    df.high      = df.high.astype("float")
-    df.low       = df.low.astype("float")
-    df.close     = df.close.astype("float")
-    df.volume    = df.volume.astype("float")
-    
-    df['adj_close'] = df['close']
-     
-    df.index = [dt.datetime.fromtimestamp(x / 1000.0) for x in df.datetime]
- 
-    return df
-
-df = get_binance_bars('BNBUSDT', '1h', dt.datetime(2020, 1, 1), dt.datetime(2020, 2, 1))
-print(df)
+client = Client(api_key, api_secret)
